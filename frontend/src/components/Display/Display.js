14a1b3285ba20cbca5dcbfdad55e5bc352fdd8e8
sunilkarki98 <suneelkarkee98@gmail.com>
2025-12-03 09:24:53 +0545

    2nd commit and checkpoint here

--- frontend/src/components/Display/Display.js
+++ frontend/src/components/Display/Display.js
@@ -1,16 +1,16 @@
 import React from "react";
 import './Display.css'
 
-export default function Display({Icon,Title,Measure}) {
+export default function Display({ Icon, Title, Measure, className }) {
   return (
     <div className="cards">
-    <button className="balance">
-      <span className="ethsymbl">
-        {Icon}
-      </span>
-      {Title} <br />
-      <span>{Measure}</span>
-    </button>
+      <button className={`balance ${className || ''}`}>
+        <span className="ethsymbl">
+          {Icon}
+        </span>
+        {Title} <br />
+        <span>{Measure}</span>
+      </button>
     </div>
   );
 }
